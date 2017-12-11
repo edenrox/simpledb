277efd3fafe4aaaa47e3f0120abc424433eeedb0
Ian Hopkins <ian@lucidhelix.com>
2017-12-10 20:29:26 -0800

    Add a test for sorting integer columns.

--- src/test/java/com/hopkins/simpledb/operations/SortTest.java
+++ src/test/java/com/hopkins/simpledb/operations/SortTest.java
@@ -1,10 +1,13 @@
 package com.hopkins.simpledb.operations;
 
 import com.hopkins.simpledb.StringRecordIteratorBuilder;
+import com.hopkins.simpledb.data.Column;
 import com.hopkins.simpledb.data.Record;
+import com.hopkins.simpledb.data.Schema;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.Arrays;
 import java.util.List;
 
 import static com.google.common.truth.Truth.assertThat;
@@ -75,6 +78,34 @@ public class SortTest {
     List<Object> names = DbIteratorUtil.readAllColumn(sort, "sname");
 
     assertThat(names).containsExactly("Robin", "Bobby", "Josiah", "Inder", "Tabitha", "Jessica").inOrder();
+
+    sort.close();
+  }
+
+  @Test
+  public void testFullResult_withIntegerColumns() {
+    Schema schema = new Schema(Arrays.asList(
+        Column.newIntColumn("year"),
+        Column.newIntColumn("month"),
+        Column.newIntColumn("id")));
+    Record record1 = new Record(schema);
+    record1.setAll(2015, 5, 4);
+    Record record2 = new Record(schema);
+    record2.setAll(2013, 1, 7);
+    Record record3 = new Record(schema);
+    record3.setAll(2013, 1, 1);
+    RecordIterator iterator = new RecordIterator(
+        schema,
+        Arrays.asList(record1, record2, record3));
+    sort =
+        new Sort(
+            iterator,
+            new SortColumn("year", SortColumn.SortDirection.ASCENDING),
+            new SortColumn("month", SortColumn.SortDirection.ASCENDING));
+    sort.open();
+
+    List<Object> ids = DbIteratorUtil.readAllColumn(sort, "id");
+    assertThat(ids).containsExactly(7, 1, 4).inOrder();
 
     sort.close();
   }
